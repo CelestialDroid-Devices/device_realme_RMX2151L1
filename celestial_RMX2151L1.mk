2a25a68bfc7e843587bceeb012305bced69e3eb9
imapotatook <imapotatook123@gmail.com>
2024-07-05 20:25:02 +0930

    Initial Celestial Changes

--- celestial_RMX2151L1.mk
+++ celestial_RMX2151L1.mk
@@ -6,6 +6,6 @@
 $(call inherit-product, device/realme/RMX2151L1/full_RMX2151L1.mk)
 
 # Inherit common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/celestial/config/common.mk)
 
-PRODUCT_NAME := lineage_RMX2151L1
+PRODUCT_NAME := celestial_RMX2151L1
